763605101f20fb91e40bd6cdace25211cc4fa249
tbbach <tiagobatistabach@gmail.com>
2022-11-04 19:31:45 -0300

    NLW Ignite Aula03

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-import { SignIn } from './src/screens/SignIn'
+import { AuthContextProvider } from './src/contexts/AuthContext'
+
+import { Pools } from './src/screens/Pools'
 import { Loading } from './src/components/Loading'
 
 import { THEME } from './src/styles/theme'
@@ -11,12 +13,15 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      { fontsLoaded ? <SignIn /> : <Loading /> }
-     </NativeBaseProvider>
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+
+        { fontsLoaded ? <Pools /> : <Loading /> }
+      </AuthContextProvider>
+    </NativeBaseProvider>
   );
 }
